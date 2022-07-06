e01d57f296c26aab34257bd37a7d8eba3ffe8a0a
jolive.hodehou <49763191+hodehoujolive@users.noreply.github.com>
2022-07-06 21:31:37 +0100

    Update test_download_file.py

--- tests/test_download_file.py
+++ tests/test_download_file.py
@@ -2,6 +2,9 @@ import unittest
 from tests.conftest import Browser
 from pages.selenium_playground_page import *
 
+import requests
+import json
+
 
 
 # I am using python unittest for asserting cases.
@@ -9,7 +12,49 @@ from pages.selenium_playground_page import *
 # If you want to run it, you should type: python <module-name.py>
 
 class TestDownloadFile(Browser):
-
+    
     def test_download(self):
         page = Selenium_Playground_Page(self.driver)
-        download_file = page.download("lorem ipsum")
+        download_file = page.download("How to download files using Selenium & Python?")
+
+class TestAPIDownloadFile():
+
+        username = "YOUR_USENAME"
+        access_Key = "YOUR_ACCESS_KEY"
+    
+        #POST_REQUEST
+        url = "https://api.lambdatest.com/automation/api/v1/user-files"
+
+        payload={}
+
+        files=[
+        ('files',('Lambdainfo.txt',open('/Users/macbookair/Downloads/Lambdainfo.txt','rb'),'text/plain'))
+        ]
+
+        headers = {
+        'authority': 'api.lambdatest.com',
+        'accept': 'application/json',
+        }
+
+        response = requests.request("POST", url, auth=(username, access_Key), headers=headers, data=payload, files=files)
+
+        print(response.text)
+    
+
+        #PUT_REQUEST
+        url = "https://api.lambdatest.com/automation/api/v1/user-files/download"
+
+
+        payload = json.dumps({
+        "key": "Lambdainfo.txt"
+        })
+
+        headers = {
+        'accept': 'application/octet-stream',
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("PUT", url, auth=(username, access_Key), headers=headers, data=payload)
+        open('/Users/macbookair/Documents/how_download_files_selenium_python/download/Lambdainfo_API.txt', 'wb').write(response.content)
+
+        print(response.text)
